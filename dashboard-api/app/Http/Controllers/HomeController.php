0de5b5cd3b77b768806287d5129941b04c744bf6
Flor Van de Kerckhove <flor.vandekerckhove>
2020-01-24 18:43:42 +0100

    save

--- dashboard-api/app/Http/Controllers/HomeController.php
+++ dashboard-api/app/Http/Controllers/HomeController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
 
 class HomeController extends Controller
 {
@@ -23,6 +26,12 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $user = Auth::user();
+        if ($user->hasAnyRole(['coach', 'admin'])){
+            return view('home');
+        } else {
+            $user->assignRole('trainee');
+            return view('welcome');
+        }
     }
 }
